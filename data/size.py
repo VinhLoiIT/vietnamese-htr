b926bea356ec9e3a4603fe6f6a98f85451de1262
Loi Ly <vinhloiit1327@gmail.com>
2020-05-13 13:29:34 +0700

    fix size.py

--- data/size.py
+++ data/size.py
@@ -5,8 +5,8 @@ from PIL import Image
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('dir', type='text')
-    parser.add_argument('output_csv', type='text', default=None)
+    parser.add_argument('dir', type=str)
+    parser.add_argument('output_csv', type=str, default=None)
     args = parser.parse_args()
 
     images = os.listdir(args.dir)
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         writer = csv.writer(f) if f else None
         writer.writerow(['path', 'w', 'h'])
         for image in images:
-            image = os.path.join(path, image)
+            image = os.path.join(args.dir, image)
             w, h = Image.open(image).size
             writer.writerow([image, w, h])
 
