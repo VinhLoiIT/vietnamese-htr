5f85710cdaf90cdba77f5d34a74b245337647ce7
Loi Ly <vinhloiit1327@gmail.com>
2020-05-16 10:21:47 +0700

    remove RandomErasing augmentation

--- system/image_transform.py
+++ system/image_transform.py
@@ -16,7 +16,6 @@ class ImageTransform(object):
                                kwargs['min_width']),
             transforms.Grayscale(3),
             transforms.ToTensor(),
-            transforms.RandomErasing() if augmentation else nn.Identity(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225]),
         ])
