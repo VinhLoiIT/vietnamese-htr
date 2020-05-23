29bde9c5359ab5a684b8f7a75f11646efe5fb05d
Loi Ly <vinhloiit1327@gmail.com>
2020-05-23 07:47:07 +0700

    convert label to string before further process

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -4,9 +4,10 @@ import os
 from PIL import Image
 from .vocab import Vocab
 
-class HTRDataset(torch.utils.data.Dataset):
 
-    def __init__(self,
+class HTRDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
         vocab: Vocab,
         image_folder: str,
         csv: str,
@@ -17,18 +18,18 @@ class HTRDataset(torch.utils.data.Dataset):
         self.image_transform = image_transform
         self.df = pd.read_csv(csv, sep='\t', keep_default_na=False)
         self.df.iloc[:, 0] = self.df.iloc[:, 0].apply(lambda filename: os.path.join(image_folder, filename))
-        self.df.iloc[:, 1] = self.df.iloc[:, 1].apply(self.vocab.process_label).apply(self.vocab.add_signals)
+        self.df.iloc[:, 1] = self.df.iloc[:, 1].astype(str).apply(self.vocab.process_label).apply(self.vocab.add_signals)
         if subset:
             self.df = self.df.sample(frac=0.1, random_state=0, axis=0)
 
     def __len__(self):
         return len(self.df)
-    
+
     def __getitem__(self, idx):
         image_path = self.df.iloc[idx, 0]
         image = Image.open(image_path).convert('L')
         image = self.image_transform(image)
-        
+
         label = torch.tensor(list(map(self.vocab.char2int, self.df.iloc[idx, 1])))
-            
+
         return image, label
